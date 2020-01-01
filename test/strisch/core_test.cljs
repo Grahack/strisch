250ad9b83662624b284720e476f321f4ec476f3f
Christophe Gragnic <cgragnic@netc.fr>
2020-01-01 14:34:20 +0100

    Remove useless mock content

--- test/strisch/core_test.cljs
+++ test/strisch/core_test.cljs
@@ -1,10 +1,4 @@
 (ns strisch.core-test
     (:require
-     [cljs.test :refer-macros [deftest is testing]]
-     [strisch.core :refer [multiply]]))
+     [cljs.test :refer-macros [deftest is testing]]))
 
-(deftest multiply-test
-  (is (= (* 1 2) (multiply 1 2))))
-
-(deftest multiply-test-2
-  (is (= (* 75 10) (multiply 10 75))))
