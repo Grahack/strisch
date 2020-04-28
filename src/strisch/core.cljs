2c87846dfec186e666db5a3033733cb4472d03c5
Christophe Gragnic <cgragnic@netc.fr>
2020-04-28 16:06:05 +0200

    Lien pentas dom et accords

--- src/strisch/core.cljs
+++ src/strisch/core.cljs
@@ -70,6 +70,17 @@
 (def GM-D [[1 5] [3 5] [4 5] [1 4] [3 4]       [0 3] [1 3] [3 3]
            [0 2] [1 2] [3 2] [1 1] [3 1] [4 1] [1 0] [3 0] [4 0]])
 
+(def P-dom-C [[0 5] [3 5] [1 4] [3 4] [0 3] [2 3]
+              [0 2] [3 2] [1 1] [3 1] [0 0] [3 0]])
+(def P-dom-A [[1 5] [4 5] [1 4] [3 4] [0 3] [3 3]
+              [1 2] [3 2] [1 1] [3 1] [1 0] [4 0]])
+(def P-dom-G [[1 5] [3 5] [0 4] [2 4] [0 3] [3 3]
+              [0 2] [2 2] [0 1] [3 1] [1 0] [3 0]])
+(def P-dom-E [[1 5] [3 5] [0 4] [3 4] [1 3] [3 3]
+              [0 2] [2 2] [1 1] [4 1] [1 0] [3 0]])
+(def P-dom-D [[1 5] [3 5] [1 4] [4 4] [1 3] [3 3]
+              [0 2] [3 2] [2 1] [4 1] [1 0] [3 0]])
+
 (def twoxfour [[0 0] [1 4] [1 4]])
 (def threexthree [[0 0] [1 3] [1 3] [1 3]])
 (def MC [[0 0] [1 3] [1 4] [1 4]])
@@ -266,12 +277,43 @@
    [:p (diagram domG [[4 3]]) (diagram domE [[2 3]])]
    [:p (diagram domC [[3 2]]) (diagram domA [[1 2]])]
    [:p (diagram domE [[4 1]]) (diagram domD [[1 1]])]
+   [:p "La penta dom sur tout le manche :"]
+   [:p (diagram sixxfour  (conj P-dom-C [1 1 5] [3 4 5]))]
+   [:p (diagram sixxfive  (conj P-dom-A [3 2 5] [1 4 5]))]
+   [:p (diagram sixxfour  (conj P-dom-G [3 0 5] [0 2 5] [3 5 5]))]
+   [:p (diagram sixxfive  (conj P-dom-E [1 0 5] [3 3 5] [1 5 5]))]
+   [:p (diagram sixxfive  (conj P-dom-D [4 1 5] [1 3 5]))]
+   [:p "La penta dom « habille » les accords 7 des cordes 2, 3 et 4 ainsi :"]
+   [:p (diagram sixxfour  (conj P-dom-C [1 1 5] [3 2 5] [2 3 5]))]
+   [:p (diagram sixxfive  (conj P-dom-A [3 1 5] [1 2 5] [3 3 5]))]
+   [:p (diagram sixxfour  (conj P-dom-G [0 1 5] [0 3 5]))]
+   [:p (diagram sixxfive  (conj P-dom-E [1 1 5] [2 2 5] [1 3 5]))]
+   [:p (diagram sixxfive  (conj P-dom-D [2 1 5] [0 2 5] [1 3 5]))]
    [:hr]
    [:h3 {:id "m7b5"} "ø"]
    [:p (diagram fourxfour [[3 1] [1 3]]) (diagram threexfive [[0 1] [3 4]])]
    [:p (diagram domD [[3 3]]) (diagram domC [[0 3]])]
    [:p (diagram domG [[3 2]]) (diagram domE [[1 2]])]
    [:p (diagram domC [[2 1]]) (diagram domA [[0 1]])]
+   [:p "La penta demi-diminuée sur tout le manche :"]
+   [:p (diagram sixxfour  (conj P-dom-G [0 1 5] [2 4 5]))]
+   [:p (diagram sixxfive  (conj P-dom-E [2 2 5] [0 4 5]))]
+   [:p (diagram sixxfive  (conj P-dom-D [3 0 5] [0 2 5] [3 5 5]))]
+   [:p (diagram sixxfour  (conj P-dom-C [0 0 5] [2 3 5] [0 5 5]))]
+   [:p (diagram sixxfive  (conj P-dom-A [3 1 5] [0 3 5]))]
+   [:p "La penta demi-diminuée « habille » les accords m7♭5 des cordes"
+       " 1, 2, 3 et 4 ainsi :"]
+   [:p (diagram sixxfour  (conj P-dom-G [1 0 5] [0 1 5] [2 2 5] [0 3 5]))]
+   [:p (diagram sixxfive  (conj P-dom-E [3 0 5] [1 1 5] [2 2 5] [1 3 5]))]
+   [:p (diagram sixxfive  (conj P-dom-D [3 0 5] [2 1 5] [3 2 5] [3 3 5]))]
+   [:p (diagram sixxfour  (conj P-dom-C [0 0 5] [0 2 5] [0 3 5]))]
+   [:p (diagram sixxfive  (conj P-dom-A [1 0 5] [1 1 5] [1 2 5] [0 3 5]))]
+   [:p "Pour les cordes 2, 3, 4 et 5 :"]
+   [:p (diagram sixxfour  (conj P-dom-G [3 1 5] [2 2 5] [3 3 5] [2 4 5]))]
+   [:p (diagram sixxfive  (conj P-dom-E [1 1 5] [0 2 5] [1 3 5] [0 4 5]))]
+   [:p (diagram sixxfive  (conj P-dom-D [2 1 5] [0 2 5] [3 3 5] [1 4 5]))]
+   [:p (diagram sixxfour  (conj P-dom-C [3 1 5] [0 2 5] [2 3 5] [1 4 5]))]
+   [:p (diagram sixxfive  (conj P-dom-A [3 1 5] [1 2 5] [3 3 5] [3 4 5]))]
    [:hr]
    [:h3 {:id "altM"} "alt M"]
    [:p (diagram fourxfour [[3 3] [0 0]]) (diagram threexfive [[0 3] [2 1]])]
